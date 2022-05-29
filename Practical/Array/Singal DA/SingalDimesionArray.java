d5edf10dec88b34ce6c75c3fd363c474ccaa1d38
Sp5455 <105196557+Sp5455@users.noreply.github.com>
2022-05-29 03:53:34 -0700

    Add files via upload

---
+++ Practical/Array/Singal DA/SingalDimesionArray.java
@@ -0,0 +1,32 @@
+package corejava1;
+
+public class SingalDimesionArray {
+	//Java Program to illustrate how to declare, instantiate, initialize  
+
+	//and traverse the Java array.  
+
+	
+
+	public static void main(String args[]){  
+
+	int a[]=new int[5];//declaration and instantiation  
+
+	a[0]=10;//initialization  
+
+	a[1]=20;  
+
+	a[2]=60;  
+
+	a[3]=800;  
+
+	a[4]=90;  
+
+	//traversing array  
+
+	for(int i=0;i<a.length;i++)//length is the property of array  
+
+	System.out.println(a[i]);  
+	}
+	}
+
+
