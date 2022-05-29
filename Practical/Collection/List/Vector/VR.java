b7b2d69e6c4e486fa91172490a98db8c69f5cddc
Sp5455 <105196557+Sp5455@users.noreply.github.com>
2022-05-29 07:00:23 -0700

    Add files via upload

---
+++ Practical/Collection/List/Vector/VR.java
@@ -0,0 +1,16 @@
+package corejava1;
+import java.util.*;
+public class VR {
+	public static void main(String args[]){  
+		Vector<String> v=new Vector<String>();  
+		v.add("Anuja");  
+		v.add("Tejas");  
+		v.add("Jayashree");  
+		v.add("Shubham");  
+
+		Iterator<String> itr=v.iterator();  
+		while(itr.hasNext()){  
+			System.out.println(itr.next());  
+		}  
+	}  
+}
