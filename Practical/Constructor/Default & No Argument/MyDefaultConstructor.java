ac69d8463a9592aacee68e363760c6f372df6c95
Sp5455 <105196557+Sp5455@users.noreply.github.com>
2022-05-29 23:56:01 -0700

    Add files via upload

---
+++ Practical/Constructor/Default & No Argument/MyDefaultConstructor.java
@@ -0,0 +1,13 @@
+package corejava1;
+
+public class MyDefaultConstructor {
+	MyDefaultConstructor()
+	{
+		System.out.println("This Is Default Constructor/ No Arguement Constructor");
+	}
+	public static void main(String[] args) {
+		
+		MyDefaultConstructor mdc = new MyDefaultConstructor();
+		
+	}
+}
