361a113a07027f1c7f4bfa780478f95b93f6483d
Sp5455 <105196557+Sp5455@users.noreply.github.com>
2022-06-15 11:22:18 -0700

    Add files via upload

---
+++ Practical/Collection/Queue/Priority Queue/PriorityQueuee.java
@@ -0,0 +1,27 @@
+package corejava1;
+
+//Java program to demonstrate the 
+	//working of PriorityQueue 
+
+import java.util.*;
+
+public class PriorityQueuee {
+	
+		// Main Method 
+		public static void main(String args[]) {
+			// Creating empty priority queue 
+			PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>(); 
+			// Adding items to the pQueue using add() 
+			pQueue.add(18); 
+			pQueue.add(20); 
+			pQueue.add(19); 
+			// Printing the top element of PriorityQueue 
+			System.out.println(pQueue.peek()); 
+			// Printing the top element and removing it 
+			// from the PriorityQueue container 
+			System.out.println(pQueue.poll()); 
+			// Printing the top element again 
+			System.out.println(pQueue.peek()); 
+			} 
+	}
+
