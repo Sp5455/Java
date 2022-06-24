c2299a23f946c3e5f370ce8f387e50a0269a0c9f
Sp5455 <105196557+Sp5455@users.noreply.github.com>
2022-06-24 13:47:54 +0530

    Add files via upload

---
+++ Practical/Thread/ThreadExOne.java
@@ -0,0 +1,17 @@
+package corejava2;
+
+public class ThreadExOne implements Runnable 
+{
+		// Initiated Run Method For Thread
+public void run() 
+{
+	System.out.println("Thread is Running Successfully...");
+}
+public static void main(String[] args) 
+{
+	ThreadExOne te=new ThreadExOne();
+	Thread t= new Thread(te);
+	//Invoke Thread
+		t.start();
+	}
+}
